76ed0cda1a55a9375d89bdf77710c31ee80bb047
vishnukkrishna <vishnukkrishna1999@gmail.com>
2023-11-11 12:34:17 +0530

    linkedlist reverse code added

--- LinkedList/singlyLinkedList.py
+++ LinkedList/singlyLinkedList.py
@@ -361,7 +361,7 @@ class LinkedList:
 
         print("Node with value", x, "not found or is the last node")
 
-
+    # LinkedList reverse
     def reverse(self):
         prev = None
         current = self.head
@@ -394,57 +394,6 @@ print()
 
 # ..................................................................
 
-# class Node:
-#     def __init__(self, data):
-#         self.data = data
-#         self.next = None
 
-# class LinkedList:
-#     def __init__(self):
-#         self.head = None
-
-#     def append(self, data):
-#         new_node = Node(data)
-#         if not self.head:
-#             self.head = new_node
-#             return
-#         last_node = self.head
-#         while last_node.next:
-#             last_node = last_node.next
-#         last_node.next = new_node
-
-#     def display(self):
-#         current_node = self.head
-#         while current_node:
-#             print(current_node.data, end=" -> ")
-#             current_node = current_node.next
-#         print("None")
-
-#     def reverse(self):
-#         prev = None
-#         current = self.head
-#         while current:
-#             next_node = current.next
-#             current.next = prev
-#             prev = current
-#             current = next_node
-#         self.head = prev
-
-# # Example usage:
-# linked_list = LinkedList()
-# linked_list.append(1)
-# linked_list.append(2)
-# linked_list.append(3)
-# linked_list.append(4)
-# linked_list.append(5)
-# linked_list.append(6)
-
-# print("Original Linked List:")
-# linked_list.display()
-
-# linked_list.reverse()
-
-# print("\nReversed Linked List:")
-# linked_list.display()
 
 
